837d02aadc5690c97e2473e61ca112d32fe1bc21
khNishad <kkhnishad@gmail.com>
2020-11-07 19:03:47 +0600

    initial

--- components/Products_category.js
+++ components/Products_category.js
@@ -53,6 +53,20 @@ const Product_Category = ()=>(
                         <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
                             <div className="width-100">
                                 <div className="pro-cat-back">
+                                    <div id="photo" style={{textAlign:" center;", padding: "20px;"}}>
+                                        <img style={{float:"left;", verticalAlign:"middle"}} src="/img/softdrinks-eecf6f54-ac79-4c09-a8f8-101a9f4f0e3e.png" width="12%" height="12%" alt="category-icon"/>
+                                        <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
+                                          Beverages
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div style={{width: "310px;"}}>
+                        <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
+                            <div className="width-100">
+                                <div className="pro-cat-back">
                                     <div id="photo" style={{textAlign: "center;", padding:" 20px;"}}>
                                         <img style={{float:"left;", verticalAlign:"middle"}} src="/img/dairy-products-7bf68a36-616a-4d34-8e30-6d61d5b2f8f2.png" width="12%" height="12%" alt="category-icon"/>
                                         <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
@@ -67,10 +81,52 @@ const Product_Category = ()=>(
                         <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
                             <div className="width-100">
                                 <div className="pro-cat-back">
+                                    <div id="photo" style={{textAlign:" center;", padding: "20px;"}}>
+                                        <img style={{float:"left;", verticalAlign:"middle"}} src="/img/softdrinks-eecf6f54-ac79-4c09-a8f8-101a9f4f0e3e.png" width="12%" height="12%" alt="category-icon"/>
+                                        <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
+                                          Beverages
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div style={{width: "310px;"}}>
+                        <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
+                            <div className="width-100">
+                                <div className="pro-cat-back">
                                     <div id="photo" style={{textAlign: "center;", padding: "20px;"}}>
                                         <img style={{float:"left;", verticalAlign:"middle" }}src="/img/planet-earth-85dac90a-72f5-42aa-8fa3-4124039b97e7.png" width="12%" height="12%" alt="category-icon"/>
                                         <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
                                         Foreign Products
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div style={{width: "310px;"}}>
+                        <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
+                            <div className="width-100">
+                                <div className="pro-cat-back">
+                                    <div id="photo" style={{textAlign: "center;", padding: "20px;"}}>
+                                        <img style={{float:"left;", verticalAlign:"middle"}} src="/img/harvest-0f4ee2d4-1370-4313-bf42-676653025494.png" width="12%" height="12%" alt="category-icon"/>
+                                        <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
+                                         Fruits & Vegetables
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div style={{width: "310px;"}}>
+                        <a className="" target="_self" href="#" style={{textDecoration:"none"}}>
+                            <div className="width-100">
+                                <div className="pro-cat-back">
+                                    <div id="photo" style={{textAlign: "center;", padding:" 20px;"}}>
+                                        <img style={{float:"left;", verticalAlign:"middle"}} src="/img/dairy-products-7bf68a36-616a-4d34-8e30-6d61d5b2f8f2.png" width="12%" height="12%" alt="category-icon"/>
+                                        <span className="product-name-style-1" style={{fontSize:"large;", verticalAlign:"middle"}}>
+                                          Dairy Products
                                         </span>
                                     </div>
                                 </div>
